2331f79b39cdf53b504344ef1a0c0b3a287e81c9
Dimitar Danailov <dimityr.danailov@gmail.com>
2017-03-01 13:22:51 +0200

    Current project has a better minification process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var vulcanize = require('gulp-vulcanize');
 var crisper = require('gulp-crisper');
+var polyclean = require('polyclean');
+var removeHtmlComments = require('gulp-remove-html-comments');
+var htmlmin = require('gulp-htmlmin');
+var minifyInline = require('gulp-minify-inline');
 
 gulp.task('copy', function() {
     return gulp.src([
@@ -19,7 +23,12 @@ gulp.task('build', function() {
             inlineScripts: true,
             inliceCss: true
         }))
+        .pipe(removeHtmlComments())
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(polyclean.leftAlignJs())
+        .pipe(polyclean.uglifyJs())
         .pipe(crisper())
+        .pipe(minifyInline())
         .pipe(gulp.dest('dist/elements'));
 });
 
